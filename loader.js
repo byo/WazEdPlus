7b2fac9ca5ced023047538182de8cf8ecf332d25
byo <byo.spoon@gmail.com>
2012-12-01 20:30:34 +0100

    Update loader to v1.2

--- loader.js
+++ loader.js
@@ -1,16 +1,14 @@
 // ==UserScript==
-// @name          Waze Aerial Shifter
-// @description   This script helps you adjust the position of underlaying bing maps
-// @include       https://world.waze.com/cartouche/
+// @name          WazeEdPlus
+// @version       1.2
+// @description   This script helps you adjust the position of underlying bing maps
+// @include       *waze.com/*
 // ==/UserScript==
 //
 // This script is just a loader for code hosted on:
 //  https://github.com/byo/WazEdPlus
 
-function load( url ) {
-	var s = document.createElement('script');
-	s.src = url;
-	document.getElmentsByTagName('head')[0].appendChild(s);
-}
-
-load('https://raw.github.com/byo/WazEdPlus/shift.js');
+var s = document.createElement('script');
+s.id = "wazedplus_root_script";
+s.src = 'https://raw.github.com/byo/WazEdPlus/stable/wazedplus.js';
+document.getElementsByTagName('head')[0].appendChild(s);

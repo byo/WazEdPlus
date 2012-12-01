59906baff9a8987a11f18028c329ddfbf3a91d7a
byo <byo.spoon@gmail.com>
2012-12-01 21:18:57 +0100

    Store shift values in cookies so that we don't lose them when refreshing

--- aerialshifter/main.js
+++ aerialshifter/main.js
@@ -12,6 +12,16 @@ $(function() {
 	var sy = nav.find('.sy');
 	$("#site-navigation").css('position', 'relative').append(nav);
 
+	var cookiePrefix = 'wazedplus_aerialshifter_';
+
+	// Restore values from cookies
+	if ($.cookie(cookiePrefix + 'sx') !== null) {
+		sx.val($.cookie(cookiePrefix + 'sx'));
+	}
+	if ($.cookie(cookiePrefix + 'sy') !== null) {
+		sx.val($.cookie(cookiePrefix + 'sy'));
+	}
+
 	var update = function() {
 
 		// Calculate meters per pixel factor of current map
@@ -25,6 +35,10 @@ $(function() {
 		$('.MicrosoftMap').css('left',
 				Math.round(shiftX / metersPerPixel) + 'px').css('top',
 				Math.round(shiftY / metersPerPixel) + 'px');
+
+		// Save values into cookies for a future use
+		$.cookie(cookiePrefix + 'sx', sx.val());
+		$.cookie(cookiePrefix + 'sy', sy.val());
 	};
 
 	update();
